472fd91c06fc2ab58ff720b33251677d360e44fc
ChipPacket <2637086@dundee.ac.uk>
2026-07-13 16:42:05 +0100

    Added a JScrollPane so that OrderList scrolls when there's a lot of items. Made orderList uneditable

--- Main.java
+++ Main.java
@@ -37,16 +37,14 @@ public class Main {
 
         // create a new text area to form the main, central part of the frame
         JTextArea orderList = new JTextArea();
+
+        //scroll pain
         JScrollPane scrollPane = new JScrollPane(orderList);
         orderList.setEditable(false);
-        scrollPane.setVerticalScrollBarPolicy(
-                JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-                scrollPane.setPreferredSize(new Dimension(250, 250)
-        );
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 
-        
         // add the text area to the centre of the frame, under the top panel
-        frame.add(orderList, BorderLayout.CENTER);
+        frame.add(scrollPane, BorderLayout.CENTER);
 
         // add a listener to the button
         btn.addActionListener(new ActionListener(){ 
